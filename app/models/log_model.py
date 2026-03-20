1a563425a7472ee6c8c05376c363b4fe79c4a11f
edi8612 <edi.sulo01@gmail.com>
2026-03-20 01:14:07 +0100

    added LogModel

--- app/models/log_model.py
+++ app/models/log_model.py
@@ -1,1 +1,16 @@
 # Here I will implement the LogModel class which will be responsible for handling log data and providing methods to analyze it.
+from pydantic import BaseModel
+from typing import Optional
+
+class LogModel(BaseModel):
+    record_id: int
+    ip: str
+    timestamp: str
+    method: str
+    endpoint: str
+    protocol: str
+    status_code: int
+    response_size: int
+    referrer: Optional[str] = None
+    user_agent: Optional[str] = None
+    extra_field: Optional[str] = None
